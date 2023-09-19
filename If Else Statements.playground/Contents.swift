c861487d4ce1f4791bbd2a63785ffa8c11ffbeb5
Sean W Taylor <142876161+Seanwt2@users.noreply.github.com>
2023-09-18 21:49:08 -1000

    Completed Parts 1-4

---
+++ If Else Statements.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+
+var goodGrade = "I’ve been doing all my work"
+
+if (goodGrade == "I'm not doing very well") {print("I should be getting an A at this point")}
+else {print("I need to work harder in this class")}
+
+var gradeA = true
+
+if (gradeA == true) {print("I really like this class")}
+else {print("I'm not so sure about coding in Swift")}
