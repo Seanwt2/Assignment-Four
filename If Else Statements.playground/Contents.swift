0ae14aa8a2c73af5d50399f0afe1c11a9c5c6414
Sean W Taylor <142876161+Seanwt2@users.noreply.github.com>
2023-09-18 22:32:12 -1000

    Completed Part 5 Problem Sets

--- If Else Statements.playground/Contents.swift
+++ If Else Statements.playground/Contents.swift
@@ -12,3 +12,34 @@ var gradeA = true
 
 if (gradeA == true) {print("I really like this class")}
 else {print("I'm not so sure about coding in Swift")}
+
+//Part 5, Problem Set (PS) 1-3
+
+//PS1
+
+var firstInt = 1
+var secondInt = 2
+
+if firstInt > secondInt {print("thats not right")}
+    
+    else {print("False")}
+
+//PS2
+
+var firstSting = "Tim "
+var secondString = "Tams"
+
+if firstSting.count == secondString.count
+{
+    print("\(firstSting)\(secondString)")
+    
+}
+
+
+//PS3
+
+var firstBoolean = true
+var secondBoolean = false
+
+if (firstBoolean == secondBoolean) {print("the truth")}
+else {print("a lie")}
